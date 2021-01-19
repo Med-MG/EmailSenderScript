af685d21d08f0b506a9b05d3caa7ab18364c4d0f
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2021-01-19 11:23:13 +0100

    Emails Class

---
+++ src/Config/emails.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Config;
+
+Class Emails {
+
+    public function GetEmails(){
+        $emailsArr = array();
+        $stream = fopen("../../emailContent/emails.txt", "r");
+        while(($line=fgets($stream))!==false) { array_push($emailsArr, $line); }
+        return $emailsArr;
+    }
+
+}
+
+
+?>
